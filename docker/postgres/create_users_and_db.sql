c798daf2a1f9f4e9536038649d8f575d90c7c7bb
rage875 <rage.875@gmail.com>
2022-02-09 13:34:43 -0600

    Docker postgres config and Database Connection manager funcionality added

---
+++ docker/postgres/create_users_and_db.sql
@@ -0,0 +1,6 @@
+\ir SQL_Scripts/database.sql;
+\connect hplussport;
+\ir SQL_Scripts/customer.sql;
+\ir SQL_Scripts/product.sql;
+\ir SQL_Scripts/salesperson.sql;
+\ir SQL_Scripts/orders.sql;
--- learn-jdbc/src/main/java/com/jdbc/learning/JDBCExecutor.java
+++ learn-jdbc/src/main/java/com/jdbc/learning/JDBCExecutor.java
@@ -1,7 +1,27 @@
 package com.jdbc.learning;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class JDBCExecutor {
-    public static void main(String... args) {
-        System.out.println("Hello learning JDBC");
+
+    public static void main(String... args){
+        DatabaseConnectionManager dcm = new DatabaseConnectionManager("localhost",
+            "hplussport", "postgres", "password");
+
+        // Code for testing Db connection and the count of records in customer table
+        try {
+            Connection connection = dcm.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT COUNT(*) FROM CUSTOMER");
+
+            while(resultSet.next()) {
+                System.out.println(resultSet.getInt(1));
+            }
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
